f637d77f5df45cab6363d4f15a5e878b977bcd9f
Nishant Dwivedi <nishant100592@gmail.com>
2023-12-24 23:57:56 +0530

    simpleFunction added

--- src/main/java/sample/SampleClass.java
+++ src/main/java/sample/SampleClass.java
@@ -4,4 +4,10 @@ public class SampleClass {
     public void simpleFunction() {
         System.out.println("This is simpleFunction");
     }
+
+    public static void main(String[] args) {
+        System.out.println("Hello agent1/ThirdDemo");
+        SampleClass obj = new SampleClass();
+        obj.simpleFunction();
+    }
 }
